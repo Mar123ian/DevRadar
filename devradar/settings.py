21c3f57aaa656f259ebee40725510c4f8ca0d12c
Mar123ian <mario.mg.sz.08@gmail.com>
2026-03-18 20:44:34 +0200

    deployment settings

--- devradar/settings.py
+++ devradar/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
 
+
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "devradar-app-database",
