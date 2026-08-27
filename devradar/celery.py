64c7ac562fc0effc2b9f11244e4339bbad6ba4eb
Mar123ian <mario.mg.sz.08@gmail.com>
2026-08-27 16:57:39 +0300

    deployment settings

--- devradar/celery.py
+++ devradar/celery.py
@@ -1,19 +1,29 @@
 import os
-from celery import Celery
 import ssl
+from celery import Celery
 
-from django.conf import settings
+# 1. Задължително в самото начало за Eventlet + psycopg2
+try:
+    from psycogreen.gevent import patch_psycopg
+    patch_psycopg()
+except ImportError:
+    pass
 
+# 2. Задаване на settings модула
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devradar.settings")
 
 app = Celery("devradar", include=['devradar.tasks', 'services.tasks'])
 
-if settings.PRODUCTION:
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# 3. Импортираме settings ТУК, след като Django е конфигуриран
+from django.conf import settings
+
+if getattr(settings, "PRODUCTION", False):
     app.conf.update(
         broker_use_ssl={'ssl_cert_reqs': ssl.CERT_NONE},
         redis_backend_use_ssl={'ssl_cert_reqs': ssl.CERT_NONE},
         broker_transport_options={'ssl': {'ssl_cert_reqs': ssl.CERT_NONE}}
     )
 
-app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
