3eac2336d1512ba18d3ccbf359540d6b3667afaf
Mar123ian <mario.mg.sz.08@gmail.com>
2026-08-28 18:43:26 +0300

    deployment settings

--- devradar/settings.py
+++ devradar/settings.py
@@ -147,12 +147,12 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-WHITENOISE_MANIFEST_STRICT = False
+# Добави и този ред за съвместимост с cloudinary_storage
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # --- CLOUDINARY CONFIGURATION ---
 CLOUDINARY_STORAGE = {
